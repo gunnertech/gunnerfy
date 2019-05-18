896e41934a2e0390aa7799e8f672f18ecc001f8a
Cody Swann <cody@gunnertech.com>
2019-05-17 22:07:55 -0400

    minor

--- src/commands/template/get.mjs
+++ src/commands/template/get.mjs
@@ -6,11 +6,7 @@ const get = ({path, projectName}) =>
   Promise.resolve("Getting template")
     .then(() => shell.exec(`
       cd ${path} &&
-      git clone --depth=1 --single-branch -b amplify-template git@github.com:gunnertech/aws-severless-react-template.git ${projectName} &&
-    `).code)
-    .then(() => shell.exec(`
-      cd ${path}/${projectName} &&
-      npm install
+      git clone --depth=1 --single-branch -b amplify-template git@github.com:gunnertech/aws-severless-react-template.git ${projectName}
     `).code)
     .then(() => setvar({path: `${path}/${projectName}`, name: `project-name-lower`, value: projectName.toLowerCase()}))
     .then(() => setvar({path: `${path}/${projectName}`, name: `project-name`, value: projectName}))
