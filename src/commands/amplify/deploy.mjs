fe66b76d722382dce11c99a63a79432e16ea140b
Cody Swann <cody@gunnertech.com>
2019-05-21 21:12:10 -0400

    minor

--- src/commands/amplify/deploy.mjs
+++ src/commands/amplify/deploy.mjs
@@ -1,6 +1,7 @@
 
 import shell from 'shelljs'
 import { projectHome, workspaceHome } from '../util'
+import fs from 'fs-extra';
 
 
 const deploy = ({projectName}) =>
@@ -20,7 +21,7 @@ cd ${projectHome(projectName)} && ${process.env.NVM_BIN}/amplify push \\
   `)
     .then(cmd => Promise.resolve(shell.exec(cmd).code))
     .then(code => code === 0 ? Promise.resolve(code) : Promise.reject("amplify deploy failed"))
-    .then(cmd => Promise.resolve(shell.exec(`
+    .then(cmd => Promise.resolve(!fs.existsSync(`${projectHome(projectName)}/src/graphql`) ? "" : shell.exec(`
       rm -rf ${projectHome(projectName)}/amplify/src/graphql &&
       mv ${projectHome(projectName)}/src/graphql ${projectHome(projectName)}/amplify/src &&
       rm -rf ${projectHome(projectName)}/src
